330e41289a6548ff0ab89aba64843416f7bde721
Tlenc <tlenciauskas@gmail.com>
2018-01-30 16:54:49 +0200

    Code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Įvedimas();
+
+        System.out.println(skaiciuotuvas.suma(a,b));
+        System.out.println(skaiciuotuvas.sandauga(a,b));
+        System.out.println(skaiciuotuvas.skirtumas(a,b));
+
+
     }
+
+    static int a;
+    static int b;
+
+    public static void Įvedimas() {
+        System.out.println("Įveskite du skaičius");
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+    }
+
 }
---
+++ src/com/company/skaiciuotuvas.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class skaiciuotuvas {
+
+
+    public static int suma(int a, int b) {
+        return (a + b);
+    }
+
+    public static int skirtumas(int a, int b) {
+        return (a - b);
+    }
+
+    public static int sandauga(int a, int b) {
+        return (a * b);
+    }
+}
